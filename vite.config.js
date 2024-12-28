13c484a7a4e11ff41e0e944ef42c42d4dfff502e
Sujal Jain <63149580+Sujal942@users.noreply.github.com>
2024-12-28 18:46:13 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Text-Editor/",
   define: {
     global: {}, // Define an empty global object to avoid circular references
   },
